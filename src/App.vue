87b8d51d957771cbfbd59161d6f70392df139d03
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-11-15 16:26:07 +0400

    done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <CustomerCreationForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomerCreationForm from './components/CustomerCreationForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomerCreationForm
   }
 }
 </script>
